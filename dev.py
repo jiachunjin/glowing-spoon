76550b8f51117395116d1f3fa7f71437acfcfa58
jiachunjin <jiachun22.jin@gmail.com>
2025-01-02 07:23:17 +0000

    reconstruction validation set

--- dev.py
+++ dev.py
@@ -32,5 +32,62 @@ def test_multi_level_interpolation(vae):
 
     return h, t
 
+def reconstruct_validation():
+    from mar_vae import AutoencoderKL
+    from autoencoder import Autoencoder_1D
+
+    import torch.nn.functional as F
+    from torchvision import transforms
+    from torchvision.datasets import ImageNet
+    from torch.utils.data import DataLoader
+    from tqdm.auto import tqdm
+    from omegaconf import OmegaConf
+    from einops import rearrange
+
+    device = torch.device('cuda:0')
+    vae = AutoencoderKL(embed_dim=16, ch_mult=(1, 1, 2, 2, 4), ckpt_path='pretrained_models/vae/kl16.ckpt').eval()
+    config = OmegaConf.load('configs/mar_vae.yaml')
+
+    autoencoder = Autoencoder_1D(config.autoencoder)
+    ckpt = torch.load('experiment/680x16_residual_bin/AE-distill_MAR_VAE-60k', map_location='cpu')
+    autoencoder.load_state_dict(ckpt, strict=True)
+    autoencoder.eval();
+    vae = vae.to(device)
+    autoencoder = autoencoder.to(device)
+
+    preprocess = transforms.Compose([
+        transforms.Resize(256, max_size=None),
+        transforms.CenterCrop(256),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True)
+    ])
+
+    val_dataset = ImageNet(root='/data/Largedata/ImageNet', split='val', transform=preprocess)
+    val_loader = DataLoader(val_dataset, batch_size=100, shuffle=False, num_workers=1)
+
+    idx_rec = 0
+
+    with torch.no_grad():
+        for images, labels in tqdm(val_loader):
+            images = images.to(device)
+            features_Bld, _ = vae.get_multi_level_features(images, [1, 2, 3, 4, 5, 6, 8, 10, 13, 16], residual=True)
+            recons = autoencoder(features_Bld)
+            
+            levels = [1, 2, 3, 4, 5, 6, 8, 10, 13, 16]
+            cur_idx = 0
+            f = torch.zeros((images.shape[0], 16, 16, 16)).to(recons.device)
+            for level in levels:
+                cur_h = rearrange(recons[:, cur_idx:cur_idx+level**2], 'b (h w) c -> b c h w', h=level)
+                up_h = F.interpolate(cur_h, size=(16, 16), mode='area')
+                f += up_h
+                cur_idx += level**2
+            recs = vae.decode(f)
+            recs = torch.clamp((recs + 1) / 2, 0, 1)
+
+            for rec in recs:
+                rec_img = ToPILImage()(rec)
+                rec_img.save(f'assets/recons/{idx_rec}.png')
+                idx_rec += 1
+
 if __name__ == '__main__':
-    test_multi_level_interpolation(vae)
+    reconstruct_validation()
