3f13e2c41bd656a30376fc93da2da71a175c8c61
jiachunjin <jiachun22.jin@gmail.com>
2025-01-18 03:41:49 +0000

    increase bsz for H100 training

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 # accelerate launch --main_process_port 29509 train.py --config 'configs/ae_find_schedule.yaml'
-# accelerate launch --main_process_port 29508 gpt_train.py
-accelerate launch --main_process_port 29507 train_ae_total.py --config 'configs/ae_total_64.yaml'
+accelerate launch --main_process_port 29508 gpt_train.py
+# accelerate launch --main_process_port 29507 train_ae_total.py --config 'configs/ae_total_64.yaml'
